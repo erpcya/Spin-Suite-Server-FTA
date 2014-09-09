00201f05b847100c69a4910b5aa93b8ee911b6e5
Yamel Senih <yamelsenih@gmail.com>
2014-09-09 21:07:31 +0200

    Add Translation

---
+++ migration/oracle/906Add_Translation_oracle.sql
@@ -0,0 +1,175 @@
+-- Sep 9, 2014 9:00:36 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET PrintName='Business Partner', XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:00:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57656
+;
+
+-- Sep 9, 2014 9:00:36 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET IsTranslated='N' WHERE AD_PrintFormatItem_ID=57656
+;
+
+-- Sep 9, 2014 9:00:51 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Productor',Updated=TO_DATE('2014-09-09 21:00:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57656 AND AD_Language='es_MX'
+;
+
+-- Sep 9, 2014 9:01:03 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET PrintName='Document Date', XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:01:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57657
+;
+
+-- Sep 9, 2014 9:01:03 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET IsTranslated='N' WHERE AD_PrintFormatItem_ID=57657
+;
+
+-- Sep 9, 2014 9:01:09 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Fecha',Updated=TO_DATE('2014-09-09 21:01:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57657 AND AD_Language='es_MX'
+;
+
+-- Sep 9, 2014 9:01:20 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsGroupBy='N', IsPageBreak='N', PrintName='Document No', SortNo=0, XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:01:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57658
+;
+
+-- Sep 9, 2014 9:01:20 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET IsTranslated='N' WHERE AD_PrintFormatItem_ID=57658
+;
+
+-- Sep 9, 2014 9:02:29 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET PrintName='No. Documento',Updated=TO_DATE('2014-09-09 21:02:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57658 AND AD_Language='es_MX'
+;
+
+-- Sep 9, 2014 9:02:39 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsGroupBy='N', IsPageBreak='N', PrintName='Comments', SortNo=0, XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:02:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57659
+;
+
+-- Sep 9, 2014 9:02:39 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET IsTranslated='N' WHERE AD_PrintFormatItem_ID=57659
+;
+
+-- Sep 9, 2014 9:02:46 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Comentarios',Updated=TO_DATE('2014-09-09 21:02:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57659 AND AD_Language='es_MX'
+;
+
+-- Sep 9, 2014 9:02:55 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsGroupBy='N', IsPageBreak='N', PrintName='Document Status', SortNo=0, XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:02:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57660
+;
+
+-- Sep 9, 2014 9:02:55 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET IsTranslated='N' WHERE AD_PrintFormatItem_ID=57660
+;
+
+-- Sep 9, 2014 9:03:00 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Estado',Updated=TO_DATE('2014-09-09 21:03:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57660 AND AD_Language='es_MX'
+;
+
+-- Sep 9, 2014 9:03:06 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='Y', XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:03:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57656
+;
+
+-- Sep 9, 2014 9:03:09 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='Y', XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:03:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57657
+;
+
+-- Sep 9, 2014 9:03:13 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='Y', IsGroupBy='N', IsPageBreak='N', SortNo=0, XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:03:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57658
+;
+
+-- Sep 9, 2014 9:03:17 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='Y', IsGroupBy='N', IsPageBreak='N', SortNo=0, XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:03:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57659
+;
+
+-- Sep 9, 2014 9:03:21 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='Y', IsGroupBy='N', IsPageBreak='N', SortNo=0, XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:03:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57660
+;
+
+-- Sep 9, 2014 9:03:37 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='Y', IsGroupBy='N', IsPageBreak='N', PrintName='Document Type', SortNo=0, XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:03:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57651
+;
+
+-- Sep 9, 2014 9:03:37 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET IsTranslated='N' WHERE AD_PrintFormatItem_ID=57651
+;
+
+-- Sep 9, 2014 9:03:45 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Tipo de Documento',Updated=TO_DATE('2014-09-09 21:03:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57651 AND AD_Language='es_MX'
+;
+
+-- Sep 9, 2014 9:03:54 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='Y', PrintName='Document Date', XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:03:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57652
+;
+
+-- Sep 9, 2014 9:03:54 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET IsTranslated='N' WHERE AD_PrintFormatItem_ID=57652
+;
+
+-- Sep 9, 2014 9:04:02 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Fecha',Updated=TO_DATE('2014-09-09 21:04:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57652 AND AD_Language='es_MX'
+;
+
+-- Sep 9, 2014 9:04:12 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='Y', IsGroupBy='N', IsPageBreak='N', PrintName='Business Partner', SortNo=0, XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:04:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57653
+;
+
+-- Sep 9, 2014 9:04:12 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET IsTranslated='N' WHERE AD_PrintFormatItem_ID=57653
+;
+
+-- Sep 9, 2014 9:04:18 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Productor',Updated=TO_DATE('2014-09-09 21:04:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57653 AND AD_Language='es_MX'
+;
+
+-- Sep 9, 2014 9:04:27 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='Y', IsGroupBy='N', IsPageBreak='N', PrintName='Farm', SortNo=0, XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:04:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57654
+;
+
+-- Sep 9, 2014 9:04:27 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET IsTranslated='N' WHERE AD_PrintFormatItem_ID=57654
+;
+
+-- Sep 9, 2014 9:04:35 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Finca',Updated=TO_DATE('2014-09-09 21:04:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57654 AND AD_Language='es_MX'
+;
+
+-- Sep 9, 2014 9:04:44 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='Y', IsGroupBy='N', IsPageBreak='N', PrintName='Comments', SortNo=0, XPosition=0, YPosition=0,Updated=TO_DATE('2014-09-09 21:04:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57655
+;
+
+-- Sep 9, 2014 9:04:44 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET IsTranslated='N' WHERE AD_PrintFormatItem_ID=57655
+;
+
+-- Sep 9, 2014 9:04:52 PM CEST
+-- Spin-Suite-FTA
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Comentarios',Updated=TO_DATE('2014-09-09 21:04:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=57655 AND AD_Language='es_MX'
+;
+
