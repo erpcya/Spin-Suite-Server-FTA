d4a5600ab55c9da24ea3531dd9562462ee4cf807
Yamel Senih <Yamel Senih@localhost>
2014-09-18 02:54:33 +0200

    Add Identifier

---
+++ migration/postgresql/908AddIdentifier_postgresql.sql
@@ -0,0 +1,15 @@
+-- Sep 18, 2014 2:50:40 AM CEST
+-- Spin-Suite-FTA
+UPDATE SPS_Column SET IsIdentifier='Y', SeqNo=2,Updated=TO_TIMESTAMP('2014-09-18 02:50:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE SPS_Column_ID=54098
+;
+
+-- Sep 18, 2014 2:51:31 AM CEST
+-- Spin-Suite-FTA
+UPDATE SPS_Column SET IsIdentifier='Y', SeqNo=3,Updated=TO_TIMESTAMP('2014-09-18 02:51:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE SPS_Column_ID=54099
+;
+
+-- Sep 18, 2014 2:51:53 AM CEST
+-- Spin-Suite-FTA
+UPDATE SPS_Column SET IsIdentifier='Y', SeqNo=4,Updated=TO_TIMESTAMP('2014-09-18 02:51:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE SPS_Column_ID=54104
+;
+
