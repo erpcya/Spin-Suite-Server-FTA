40dc4ee31f261ae3719ecabe49281a49a889716e
Yamel Senih <Yamel Senih@localhost>
2014-10-02 12:09:33 +0200

    Add Tables

---
+++ migration/oracle/911Change_Identifier_oracle.sql
@@ -0,0 +1,10 @@
+-- 01-oct-2014 10:11:28 CEST
+-- Spin-Suite-FTA
+UPDATE SPS_Column SET IsIdentifier='N',Updated=TO_DATE('2014-10-01 10:11:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE SPS_Column_ID=53928
+;
+
+-- 01-oct-2014 10:11:47 CEST
+-- Spin-Suite-FTA
+UPDATE SPS_Column SET IsIdentifier='Y', SeqNo=2,Updated=TO_DATE('2014-10-01 10:11:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE SPS_Column_ID=53931
+;
+
